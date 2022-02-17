0bd6901290e8ba4816c27d7a63d9f7399fb7bc0e
Dylan Cross <dylan@sitehost.co.nz>
2022-02-17 16:08:44 +1300

    modify build script to trigger deploy

--- buildtest.sh
+++ buildtest.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 cd /var/www
-echo "Cleanup"
-rm -rf generated_dir
 echo "Running mkdir generated_dir"
 mkdir generated_dir
-echo "cd generated_dir"
-cd generated_dir
-echo "mkdir dir2"
-mkdir dir2
-echo "What is here?"
-ls -la
-echo "What about here?"
-cd ..
-ls -la
+echo "Touching file inside generated_dir"
+tocuh generated_dir/file.txt
 
