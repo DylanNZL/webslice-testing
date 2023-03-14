492a6cc70ad2cb5865898f58d6ea8e53cc3c1fff
Dylan Cross <dylnnzl@gmail.com>
2023-03-14 15:26:47 +1300

    Update buildtest.sh

--- buildtest.sh
+++ buildtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running mkdir generated_dir"
-mkdir generated_dir
+mkdir /var/www/public/generated_dir
 echo "Touching file inside generated_dir"
-touch generated_dir/file.txt
+echo "Build script generated file" > /var/www/public/generated_dir/file.txt
 
